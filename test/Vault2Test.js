44fe5a4d9bbc3515c25a163a76cfbfbda7bcde1f
Raghuramkowdeed <raghuramkowdeed@gmail.com>
2022-01-21 23:07:54 -0800

    Update Vault2Test.js

--- test/Vault2Test.js
+++ test/Vault2Test.js
@@ -4,10 +4,23 @@ const { ethers } = require("hardhat");
 describe("Vault 2", () => {
 	beforeEach(async () => {
 		Vault2 = await ethers.getContractFactory("Vault2");
-		[owner] = await ethers.getSigners();
+		[owner, adr1] = await ethers.getSigners();
 
 		vault2 = await Vault2.deploy();
 
 		await vault2.deployed();
 	});
+
+
+	it('mint coins', async () => {
+		await vault2.connect(owner).mint({value: 1});
+		expect(await vault2.balanceOf(owner.address)).to.equal(1);
+	});
+
+	it('burn coins', async () => {
+		await vault2.connect(owner).mint({value: 3});
+		await vault2.connect(owner).burn(2);
+		expect(await vault2.balanceOf(owner.address)).to.equal(1);
+	});
+
 });
